211321aa9bbda3f668d4782faad821d9c6a725c3
Navin Kanna <navinkumark1997@gmail.com>
2021-09-21 00:06:37 +0530

    Initial Changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NotesListComponent } from './pages/notes-list/notes-list.component';
+import { MainLayoutComponent } from './pages/main-layout/main-layout.component';
+import { NoteCardComponent } from './note-card/note-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotesListComponent,
+    MainLayoutComponent,
+    NoteCardComponent
   ],
   imports: [
     BrowserModule,
